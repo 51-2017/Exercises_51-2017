da895a7d06e4d8aef0a234c85c56af73b49e79c6
Matija Ljujic - 51/2017 <matija.ljujic98@gmail.com>
2020-11-09 19:41:17 +0100

    Kreiran ListBox sa podacima

--- Exercises_51-2017/Exercises_51-2017/Form1.cs
+++ Exercises_51-2017/Exercises_51-2017/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Data.SqlClient;
+
 namespace Exercises_51_2017
 {
     public partial class Form1 : Form
@@ -17,5 +19,47 @@ namespace Exercises_51_2017
             InitializeComponent();
         }
 
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<ExerciseResult> lista = new List<ExerciseResult>();
+            using (SqlConnection konkcija = new SqlConnection())
+            {
+                konkcija.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=FacultyDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                konkcija.Open();
+                string q = "select* from ExerciseResults";
+
+                SqlCommand sqlCommand = new SqlCommand(q, konkcija);
+                using (SqlDataReader citac = sqlCommand.ExecuteReader())
+                {
+
+
+                    while (citac.Read())
+                    {
+                        ExerciseResult exerciseResult = new ExerciseResult();
+                        exerciseResult.Id = (int)citac.GetInt32(0);
+                        exerciseResult.StudentName = citac.GetString(1);
+                        exerciseResult.StudentIndex = citac.GetString(2);
+                        exerciseResult.Point = (int)citac.GetInt32(3);
+                        lista.Add(exerciseResult);
+                    }
+
+
+                }
+
+
+            }
+            listBox1.Items.Clear();
+            foreach (ExerciseResult exerciseResult1 in lista)
+            {
+                listBox1.Items.Add(exerciseResult1.StudentName + " " +
+                    exerciseResult1.StudentIndex + " " + exerciseResult1.Point);
+            }
+        }
+            
     }
 }
